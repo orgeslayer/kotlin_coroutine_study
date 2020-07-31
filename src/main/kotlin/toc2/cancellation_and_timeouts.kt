76be30c68f16bc7257979f0ec93430be0634e6e5
orgeslayer <orgeslayer@gmail.com>
2020-07-31 10:28:27 +0900

    Coroutine Guide - Cancellation and Timeouts
    rename package

---
+++ src/main/kotlin/toc2/cancellation_and_timeouts.kt
@@ -0,0 +1,17 @@
+package toc2
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val job = launch {
+        repeat(1000) { i ->
+            println("job: I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancel() // cancels the job
+    job.join() // waits for job's completion
+    println("main: Now I can quit.")
+}
